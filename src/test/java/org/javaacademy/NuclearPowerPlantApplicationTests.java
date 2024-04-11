5c719b8b4fe789ce87dff4f756728ed22c4002ae
kibo <sm6078@mail.ru>
2024-04-11 20:19:11 +0300

    create structure project

---
+++ src/test/java/org/javaacademy/NuclearPowerPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuclearPowerPlantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
